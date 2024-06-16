0b3cecee67c743651176f51f1612e16ec779966a
Oscargil80 <oscargil80@gmail.com>
2024-06-15 22:54:40 -0400

    Terminando el Ejemplo de leonardo morales de Live Data y View model

--- app/src/main/java/com/oscargil80/viewmodelleonardo/MainActivity.kt
+++ app/src/main/java/com/oscargil80/viewmodelleonardo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.oscargil80.viewmodelleonardo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.oscargil80.viewmodelleonardo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+
+    private  val viewModel  = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+      binding.btnInc.setOnClickListener {
+          viewModel.incrementarValor()
+      }
+
+
+        binding.btnDec.setOnClickListener {
+            viewModel.decrementarValor()
+        }
+
+        viewModel.count.observe(this){ countValue ->
+            binding.tvValor.text = countValue.toString()
+        }
+
+
     }
 }
--- app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
+++ app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
@@ -1,4 +1,29 @@
 package com.oscargil80.viewmodelleonardo
 
-class MainViewModel {
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class MainViewModel:ViewModel() {
+
+    private var _count  = MutableLiveData<Int>()
+    val count: LiveData<Int> = _count
+
+    init {
+        _count.value = 0
+        incrementarValor()
+    }
+
+
+    fun incrementarValor(){
+           _count.value = _count.value?.plus(1)
+        }
+
+    fun decrementarValor(){
+        _count.value = _count.value?.minus(1)
+    }
+
+
 }
