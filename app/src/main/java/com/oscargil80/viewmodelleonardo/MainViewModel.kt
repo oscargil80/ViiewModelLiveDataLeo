32c466ce04804144104a7171e53082096879dadf
Oscargil80 <oscargil80@gmail.com>
2024-06-17 00:11:09 -0400

    Ejemplo de Shared Flow, es muy sencillo pense que era otra cosa, lol

--- app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
+++ app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
@@ -4,25 +4,38 @@ package com.oscargil80.viewmodelleonardo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
 
-    private var _count  = MutableLiveData<Int>()
-    val count: LiveData<Int> = _count
+    private var _count = MutableStateFlow<Int>(0)
+    val count: StateFlow<Int> = _count
+
+    private var _event = MutableSharedFlow<Boolean>()
+    val event = _event.asSharedFlow()
 
     init {
-        _count.value = 0
-
     }
 
 
-    fun incrementarValor(){
-           _count.value = _count.value?.plus(1)
-        }
+    fun incrementarValor() {
+        _count.value = _count.value.plus(1)
+    }
 
-    fun decrementarValor(){
-        _count.value = _count.value?.minus(1)
+    fun decrementarValor() {
+        if (_count.value > 0) {
+            _count.value = _count.value.minus(1)
+        } else {
+            viewModelScope.launch {
+                _event.emit(true)
+            }
+        }
     }
 
 
