b49349e6301d79d303b11516f5e784aab04390b1
Oscargil80 <oscargil80@gmail.com>
2024-06-17 23:54:32 -0400

    Ejercicio de conteo regresivo con view model, De leonardo Morales <3

--- app/src/main/java/com/oscargil80/viewmodelleonardo/MainActivity.kt
+++ app/src/main/java/com/oscargil80/viewmodelleonardo/MainActivity.kt
@@ -2,32 +2,75 @@ package com.oscargil80.viewmodelleonardo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import com.oscargil80.viewmodelleonardo.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding :ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
 
-    private  val viewModel  = MainViewModel()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-      binding.btnInc.setOnClickListener {
-          viewModel.incrementarValor()
-      }
+        binding.btnInc.setOnClickListener { viewModel.incrementarValor() }
 
+        binding.btnDec.setOnClickListener { viewModel.decrementarValor() }
 
-        binding.btnDec.setOnClickListener {
-            viewModel.decrementarValor()
+        binding.btnStart.setOnClickListener { viewModel.displayStartCount() }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.maxProgres.collect {
+                binding.circularProgressIndicator.max = it
+            }
         }
 
-        viewModel.count.observe(this){ countValue ->
-            binding.tvValor.text = countValue.toString()
+        lifecycleScope.launchWhenStarted {
+            viewModel.StartCount.collect{
+                Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.isRunnig.collect { isRunning ->
+                if (isRunning) {
+                    binding.btnInc.isEnabled = false
+                    binding.btnDec.isEnabled = false
+                    binding.btnStart.isEnabled = false
+                } else {
+                    binding.btnInc.isEnabled = true
+                    binding.btnDec.isEnabled = true
+
+
+                    binding.circularProgressIndicator.max = 100
+                    binding.circularProgressIndicator.progress = 100
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.time.collect { timeValue ->
+                binding.tvValor.text = timeValue.toString()
+
+                if(timeValue == 0){
+                    viewModel.restartTimer()
+                }
+
+                if(!viewModel.isRunnig.value){
+                    binding.btnStart.isEnabled = timeValue >0
+                }else{
+                    binding.circularProgressIndicator.progress = timeValue
+                }
+
+            }
         }
 
 
--- app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
+++ app/src/main/java/com/oscargil80/viewmodelleonardo/MainViewModel.kt
@@ -1,28 +1,62 @@
 package com.oscargil80.viewmodelleonardo
 
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
 
-    private var _count  = MutableLiveData<Int>()
-    val count: LiveData<Int> = _count
+    private var _time = MutableStateFlow<Int>(0)
+    val time: StateFlow<Int> = _time
 
-    init {
-        _count.value = 0
+    private val _StartCount = MutableSharedFlow<Int>()
+    val StartCount = _StartCount.asSharedFlow()
 
+    private var  _maxProgres = MutableStateFlow<Int>(0)
+    val maxProgres:StateFlow<Int> = _maxProgres
+
+    private var _isRunning = MutableStateFlow<Boolean>(false)
+    val isRunnig:StateFlow<Boolean> = _isRunning
+
+
+    fun incrementarValor() {
+        _time.value = _time.value.plus(1)
     }
 
-
-    fun incrementarValor(){
-           _count.value = _count.value?.plus(1)
+    fun decrementarValor() {
+        if(_time.value >0) {
+            _time.value = _time.value.minus(1)
         }
+    }
 
-    fun decrementarValor(){
-        _count.value = _count.value?.minus(1)
+    fun displayStartCount() {
+        (3 downTo 0).asFlow().onEach {
+            if (it > 0) {
+                _StartCount.emit(it)
+                delay(1000)
+            } else {
+                startTimer()
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun startTimer() {
+        _maxProgres.value = _time.value
+        _isRunning.value = true
+
+
+        (_time.value downTo 0).asFlow().onEach {
+            _time.value = it
+            delay(1000)
+        }.launchIn(viewModelScope)
+    }
+
+    fun restartTimer(){
+        _maxProgres.value = 0
+        _isRunning.value = false
+        _time.value = 0
     }
 
 
